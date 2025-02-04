6ca8061ee62309cebd2d5d875573b1036211f5d1
AnastasiiaShelestian <shelestnest@gmail.com>
2025-02-04 22:26:40 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+echo "Hello, World with echo!<br>";
+print "Hello, World with print! <br>";
+
+$days = 288;
+$message = "Все возвращаются на работу!";
+echo "days = $days <br>";
+echo "message = $message <br>"; 
+echo "Прошло уже " . "$days " . "поэтому " . $message;
+?>
+
+
+
+
+
+
