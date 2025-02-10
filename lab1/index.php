58a994e37d520e17ec9cec470f420f0afc355f3b
AnastasiiaShelestian <shelestnest@gmail.com>
2025-02-10 11:40:39 +0200

    adding folder

---
+++ lab1/index.php
@@ -0,0 +1,18 @@
+<?php
+echo "Hello, World with echo!<br>";
+print "Hello, World with print! <br>";
+
+
+
+$days = 288;
+$message = "Все возвращаются на работу!";
+echo "days = $days <br>";
+echo "message = $message <br>"; 
+echo "Прошло уже " . "$days " . "поэтому " . $message;
+
+
+
+
+
+
+
